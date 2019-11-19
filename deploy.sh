290e8a70dadc1c8542f80bb96c4fa840abca730b
sanvi <lucanwei@shouqianba.com>
2019-11-19 16:53:02 +0800

    first commit

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
-hexo generate
+hexo clean
+hexo g
 cp -R public/* .deploy/sanvibyfish.github.com
 cd .deploy/sanvibyfish.github.com 
 git add .
 git commit -m “update”
 git push origin master
-
+hexo deploy
